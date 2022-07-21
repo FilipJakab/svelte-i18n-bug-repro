67e54a6a493c1109d9982839a07ee545b2165e67
Filip Jakab <jakabfilip.fj@gmail.com>
2022-07-22 01:04:53 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import {onMount} from "svelte";
+  import {isLoading, _} from "svelte-i18n"
+  import "./i18n"
+</script>
+
+<main>
+  <h1>Vite + Svelte</h1>
+
+  {#if !$isLoading}
+    <div class="card">
+      <p>
+        Working:
+        {$_("common.workingMessage", {values: {name: "John"}})}
+      </p>
+
+      <p>
+        Not working:
+        {$_("common.notWorkingMessage", {values: {name: "John"}})}
+      </p>
+    </div>
+  {/if}
+</main>
